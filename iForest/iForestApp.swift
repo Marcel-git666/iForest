c85866e516127434ba103ab9e6c0df7a8881dfee
Marcel Mravec <marcel.mravec@gmail.com>
2024-09-04 16:39:56 +0200

    initial files

---
+++ iForest/iForestApp.swift
@@ -0,0 +1,17 @@
+//
+//  iForestApp.swift
+//  iForest
+//
+//  Created by Marcel Mravec on 04.09.2024.
+//
+
+import SwiftUI
+
+@main
+struct iForestApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
